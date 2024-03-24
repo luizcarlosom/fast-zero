997b3e8b5ee12cc913dfe84fdb9ffe0aeb4b5783
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-03-24 15:03:04 -0300

    feat: integrating sqlite database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -22,18 +25,29 @@ def test_create_user(client):
     }
 
 
+def test_create_user_invalid(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username already registered'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
 
 
 def test_invalid_update_user(client):
@@ -49,7 +63,7 @@ def test_invalid_update_user(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -66,12 +80,12 @@ def test_update_user(client):
     }
 
 
-def test_user_id_read(client):
+def test_user_id_read(client, user):
     response = client.get('/users/1')
     assert response.status_code == 200
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
+        'username': 'Teste',
+        'email': 'teste@test.com',
         'id': 1,
     }
 
@@ -89,7 +103,7 @@ def test_invalid_delete_user(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == 200
