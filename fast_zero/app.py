5683d52834adf59e33ab7d102ebfc26ee587a7da
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-04-09 21:02:48 -0300

    feat: implement get, delete and patch endpoints for todos

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from fast_zero.routes import auth, users
+from fast_zero.routes import auth, todos, users
 from fast_zero.schemas import Message
 
 app = FastAPI()
 
+app.include_router(todos.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 
